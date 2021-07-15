0deaef455be1e51b3ae37915ffae3bca9562523c
FranArnaudo <franarnaudo1999@gmail.com>
2021-07-15 15:19:02 -0300

    merge a fran desde el main

---
+++ src/components/albumes/SubidaFotos.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const SubidaFotos = () => {
+
+    return ( 
+        <div className="container-fluid">
+            <h1>Subida fotos</h1>
+        </div>
+     );
+}
+ 
+export default SubidaFotos;
