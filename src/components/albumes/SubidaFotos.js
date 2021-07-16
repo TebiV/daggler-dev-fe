eef8b44eb99d400d87cf378e55281e5599d74520
FranArnaudo <franarnaudo1999@gmail.com>
2021-07-16 13:00:43 -0300

    carousel

--- src/components/albumes/SubidaFotos.js
+++ src/components/albumes/SubidaFotos.js
@@ -1,6 +1,6 @@
 import React,{useState, useEffect} from 'react';
 import '../../css/SubidaFotos_css.css'
-
+import Slider from "react-slick";
 
 
 
@@ -9,14 +9,35 @@ const SubidaFotos = () => {
 
     //*State de fotos
     const [fotos, setFotos] = useState([])
-    //! Estuve viendo y no se hacer la preview de las fotos desde el input. No les puedo getear la url provista por la pc. 
-    //TODO: Falta implementar el carrusel de fotos, lo podemos hacer mediante react-slick es facil. 
-    //TODO: Ver como solucionamos el tema de obtener la url de las fotos las opciones son:
-        //* Subirlas al backend, que el backend me pase una url y yo la muestro, eso si se como hacerlo (no la parte del backend)
-        //* Seguir renegando con el url desde el input, yo no lo se hacer, le pregunte a nico y me dijo que no lo hizo nunca, que se tendria que poner a verlo
 
-    //? No estoy seguro, pero creo que puedo hacer para que las imagenes se muestren en la misma pagina si se suben al backend, el problema quizas es 
-    //? que hay llamadas extras al backend y es mas costoso para el Leo...
+    const [preview_urls, set_preview_urls] = useState([]);
+    const handleChange = (e: React.ChangeEvent) => {
+    set_preview_urls((prev) =>
+      prev.concat(
+        Array.from(e.target.files ?? []).map((f) =>
+          window.URL.createObjectURL( f )
+                )
+            )
+        );
+    };
+  useEffect(() => {
+    return () => {
+      preview_urls.forEach((url) => window.URL.revokeObjectURL(url));
+    };
+  }, []);
+
+    var settings = {
+        dots: true,
+        infinite: false,
+        speed: 100,
+        slidesToShow: 5,
+        slidesToScroll: 15,
+        centerMode: true,
+        focusOnSelect: true,
+        swipeToSlide:true
+        
+    };
+
     return ( 
         <div className="container-fluid">
 
@@ -32,14 +53,19 @@ const SubidaFotos = () => {
                         type="file"
                         id="inputFotos"
                         multiple
-                        
+                        onChange={handleChange}
                         accept=".png, .jpg, .raw"
                     />
 
                 </div>
                 <div className="col-xs-12 col-md-9">
-                  
-
+                    <Slider {...settings} className="mx-4">
+                        {preview_urls.map((url) => (
+                        <div>
+                            <img key={url} src={url} alt="" className="preview"></img>
+                        </div>
+                    ))}
+                    </Slider>
                 </div>
             </div>
 
