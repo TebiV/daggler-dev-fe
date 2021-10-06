13c22828342f64fdaa1e336b5c13eefb95ddff95
julb2 <julianismael13@gmail.com>
2021-10-06 11:45:27 -0300

    mas fixes

--- src/components/layout/ModalConfirmacion.js
+++ src/components/layout/ModalConfirmacion.js
@@ -4,29 +4,29 @@ import '../../css/Error_css.css'
 const ModalConfirmacion = ({ album, onEliminar }) => {
     return (
         <>
-            <div class="modal fade" id="ModalConfirmEliminarAlbum" tabindex="-1" aria-labelledby="ModalConfirmEliminarAlbumLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered" role="document">
-                    <div class="modal-content">
+            <div className="modal fade" id="ModalConfirmEliminarAlbum" tabIndex="-1" aria-labelledby="ModalConfirmEliminarAlbumLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered" role="document">
+                    <div className="modal-content">
 
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="ModalConfirmEliminarAlbumLabel">{album.name}</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="ModalConfirmEliminarAlbumLabel">{album.name}</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
 
-                        <div class="modal-body text-center">
+                        <div className="modal-body text-center">
                             ¿Desea eliminar <b>permanentemente</b> este álbum?
                         </div>
 
-                        <div class="modal-footer">
+                        <div className="modal-footer">
                             <button
                                 type="button"
-                                class="btn btn-warning"
+                                className="btn btn-warning"
                                 data-bs-dismiss="modal"
                             >Cancelar</button>
 
                             <button
                                 type="button"
-                                class="btn btn-outline-danger"
+                                className="btn btn-outline-danger"
                                 data-bs-dismiss="modal"
                                 onClick={() => onEliminar(album._id)}
                             >Eliminar</button>
